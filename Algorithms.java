b3b0e1a76a50ba3452070ee59e53d484eb06c986
Victoriashulman <victorias252@nycstudents.net>
2024-12-11 16:48:19 +0000

    commit

--- Algorithms.java
+++ Algorithms.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
+public class Algorithms {
+    private static File f;
+    private static Scanner s;
+    public static void main(String[] args) throws FileNotFoundException {
+        f = new File("words.txt");
+        int two = two();
+        int temp = temp();
+        System.out.println(two);
+        System.out.println(temp);
+        s.close();}
+
+ public static int two() throws FileNotFoundException{
+        s = new Scanner(f);
+        int two = 0;
+        int temp = 0;
+        while (s.hasNext()) {
+             temp = s.next();
+            if(temp.length == 2){
+                two ++;
+            }
+            return two;}}}
+                
